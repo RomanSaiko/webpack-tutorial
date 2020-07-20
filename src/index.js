7b8d5e8a8a819488fb7db799eab396c9a1e272b6
RomanSaiko <rsaiko1995@gmail.com>
2020-07-20 22:20:46 +0300

    + xml, csv config;

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Post from './Post'
 import './styles/styles.css'
-import json from './assets/json'
 import WebpackLogo from './assets/webpack-logo.png'
+// import json from './assets/json'
+// import xml from './assets/data.xml'
+// import csv from './assets/data.csv'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
-console.log('post to string', post.toString())
+console.log('post to string:', post.toString())
 
-console.log('JSON:', json)
+// console.log('JSON:', json)
+// console.log('XML:', xml)
+// console.log('CSV:', csv)
