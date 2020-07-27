b90c8e7b2a9383172eaf15d71987d4d0eb07f465
RomanSaiko <rsaiko1995@gmail.com>
2020-07-27 20:26:16 +0300

    + less-loader; + sass-loader;

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import * as $ from 'jquery'
 import Post from '@models/Post'
-import './styles/styles.css'
 import WebpackLogo from './assets/webpack-logo.png'
 // import json from './assets/json'
 // import xml from './assets/data.xml'
 // import csv from './assets/data.csv'
+import './styles/styles.css'
+import './styles/less.less'
+import './styles/scss.scss'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
